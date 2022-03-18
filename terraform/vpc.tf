7ba29719fe7aa34ea4d916b1595a2a47986c0737
Iliyan Stoimenov <istoimenov@objectsystems.com>
2022-03-18 17:49:27 +0200

    fixes from terraform apply

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,8 +1,32 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "main"
+    cidr_block = "10.0.0.0/16"
+    tags       = {
+      Name = "main"
+    }
   }
-}
+
+  resource "aws_subnet" "main" {
+    vpc_id     = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    tags       = {
+      Name = "subnet_1"
+    }
+  }
+
+  resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+  }
+
+  resource "aws_route_table" "main" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.main.id
+    }
+  }
+
+  resource "aws_route_table_association" "main" {
+    subnet_id      = aws_subnet.main.id
+    route_table_id = aws_route_table.main.id
+  }
