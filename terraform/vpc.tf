b3145145bf3271e8203017b5fe2daeb32856dddc
Iliyan Stoimenov <istoimenov@objectsystems.com>
2022-03-22 22:04:02 +0200

    changes to vpc, eks

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,36 +1,3 @@
-# resource "aws_vpc" "main" {
-#     cidr_block = "10.0.0.0/16"
-#     tags       = {
-#       Name = "main"
-#     }
-#   }
-
-#   resource "aws_subnet" "main" {
-#     vpc_id     = aws_vpc.main.id
-#     cidr_block = "10.0.1.0/24"
-#     tags       = {
-#       Name = "subnet_1"
-#     }
-#   }
-
-#   resource "aws_internet_gateway" "main" {
-#     vpc_id = aws_vpc.main.id
-#   }
-
-#   resource "aws_route_table" "main" {
-#     vpc_id = aws_vpc.main.id
-
-#     route {
-#       cidr_block = "0.0.0.0/0"
-#       gateway_id = aws_internet_gateway.main.id
-#     }
-#   }
-
-#   resource "aws_route_table_association" "main" {
-#     subnet_id      = aws_subnet.main.id
-#     route_table_id = aws_route_table.main.id
-#   }
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -52,15 +19,6 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
-  # public_subnet_tags = {
-  #  # public_subnet = "shared"
-  #   "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-  #   "kubernetes.io/role/elb"  = "1"
-  # }
-  # private_subnet_tags = {
-  #   "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-  #   "kubernetes.io/role/internal-elb"  = "1"
-  # }
 }
 
 resource "aws_eip" "nat_gw_elastic_ip" {
